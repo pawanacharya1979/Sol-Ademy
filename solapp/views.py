b063f4d889e2c02983f19960cc4e7d88e4ad66c4
Sunera <pawanacharya1979@gmail.com>
2019-04-25 02:00:11 +0530

    Adding contact page

--- solapp/views.py
+++ solapp/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
+from .forms import ContactForm
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.core.mail import EmailMessage
+
 # Create your views here.
 
 
@@ -21,4 +24,25 @@ def gallery(request):
 
 
 def contactus(request):
-    return render(request, 'solapp/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            name = form.cleaned_data.get('Name')
+            email = form.cleaned_data.get('Email')
+            phone = form.cleaned_data.get('phone_number')
+            subject = form.cleaned_data.get('subject')
+            message = form.cleaned_data.get('Message')
+            body = {'Name':name,'Email':email,'phone_number':phone,'subject':subject,'Message':message},
+
+            email = EmailMessage(
+                'Contact From',
+                str(body),
+                to=['pawanacharya1979@gmail.com']
+            )
+            email.send()
+            messages.success(request, 'Thank You! for contacting us, we will get back to you soon.')
+            return HttpResponseRedirect('/contact/')
+    else:
+        form = ContactForm()
+    return render(request, 'solapp/contact.html', {'form': form})
