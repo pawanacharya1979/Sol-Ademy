85f01f3c17434f68f2d96d8cc92731e0ff9f18c0
Sunera <pawanacharya1979@gmail.com>
2019-04-25 14:29:38 +0530

    Adding media changes

--- solapp/urls.py
+++ solapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
        url(r'^$', views.home, name='home'),
        url(r'^about-us/$', views.about, name='about'),
-       url(r'^contact/$', views.news, name='news'),
+       url(r'^news/$', views.news, name='news'),
        url(r'^gallery/$', views.gallery, name='gallery'),
        url(r'^contact-us/$', views.contactus, name='contactus'),
        ]
